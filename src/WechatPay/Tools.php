7d059eb97d6fc9e6991dc2ffb08bac2d156c370e
moon2050 <117908549@qq.com>
2019-12-12 11:05:23 +0800

    yue

---
+++ src/WechatPay/Tools.php
@@ -0,0 +1,16 @@
+<?php
+namespace RedPay\WechatPay;
+/**
+ * 工具类
+ */
+class Tools {
+
+	public function __constructor() {
+
+	}
+
+	public static function sayHello () {
+		echo "hello world";
+	}
+
+}
